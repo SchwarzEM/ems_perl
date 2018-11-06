8a3053657747bedce3505daad27c144f48fdad4c
Erich Schwarz <ems394@cornell.edu>
2018-11-05 23:32:22 -0500

    Add process_uniprot2gene_05nov2018.pl to cons_unks.

---
+++ cons_unks/process_uniprot2gene_05nov2018.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+use List::MoreUtils qw(uniq);
+
+my $infile = q{};
+my $prefix = q{};
+
+$infile = $ARGV[0] if $ARGV[0];
+$prefix = $ARGV[1] if $ARGV[1];
+
+open my $INFILE, '<', $infile;
+while (my $input = <$INFILE>) {
+    chomp $input;
+    if ( $input =~ /\A [a-z]+\|(\S+)\| [^\t]* \t (.*) \z/xms ) {
+        my $uniprot        = $1;
+        my $raw_gene_data  = $2;
+        my @raw_gene_texts = split /\t/, $raw_gene_data;
+        my @gene_names     = ();
+
+        foreach my $raw_gene_text (@raw_gene_texts) {
+            if ( $raw_gene_text =~ / gene:(\S+) .+ gene_symbol:(\S+) /xms ) {
+                my $gene_id   = $1;
+                my $gene_name = $2;
+                my $gene     = "$prefix|$gene_id|$gene_name";
+                push @gene_names, $gene;
+            }
+            elsif ( $raw_gene_text =~ / gene:(\S+) /xms ) {
+                my $gene_id = $1;
+                my $gene    = "$prefix|$gene_id";
+                push @gene_names, $gene;
+            }
+            else {
+                die "Can't parse gene info in: $raw_gene_text\n";
+            }
+        }
+        @gene_names = sort @gene_names;
+        @gene_names = uniq @gene_names;
+        my $gene_list = join '; ', @gene_names;
+        print "$uniprot\t$gene_list\n";
+    }
+}
+close $INFILE;
+
+
