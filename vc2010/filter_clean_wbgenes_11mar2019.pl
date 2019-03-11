099df04e690b876f36d37e8e71590cec7bd3d23a
Erich Schwarz <ems394@cornell.edu>
2019-03-11 16:40:15 -0400

    Add optional third *bad* argument: this selects obverse of good gene annotations for a filtered GFF.

--- vc2010/filter_clean_wbgenes_11mar2019.pl
+++ vc2010/filter_clean_wbgenes_11mar2019.pl
@@ -6,11 +6,13 @@ use autodie;
 
 my $gff = q{};
 my $ids = q{};
+my $bad = q{};
 
 my %good_id = ();
 
 $gff = $ARGV[0] if $ARGV[0];
 $ids = $ARGV[1]	if $ARGV[1];
+$bad = $ARGV[2] if $ARGV[2];
 
 open my $IDS, '<', $ids;
 while ( my $input = <$IDS> ) { 
@@ -27,7 +29,10 @@ while ( my $input = <$GFF> ) {
     }
     elsif ( $input =~ /\A (?:[^\t]* \t){8} gene_id [ ] \" (\S+) \"/xms ) { 
         my $gene_id = $1;
-        if ( exists $good_id{$gene_id} ) {
+        if ( exists $good_id{$gene_id} and (! $bad ) ) {
+            print "$input\n";
+        }
+        elsif ( $bad and (! exists $good_id{$gene_id} ) ) {
             print "$input\n";
         }
     }
