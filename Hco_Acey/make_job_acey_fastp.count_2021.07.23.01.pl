d9aa9099c45e0d471e670aaf0233f457b7b4890e
Erich Schwarz <ems394@cornell.edu>
2021-07-23 16:16:22 -0400

    Revise to make _1 selections more explicit, and to make it easier to recode this for paired-end fastp jobmaking.

--- Hco_Acey/make_job_acey_fastp.count_2021.07.23.01.pl
+++ Hco_Acey/make_job_acey_fastp.count_2021.07.23.01.pl
@@ -10,25 +10,25 @@ use File::Spec::Functions;  # catdir, catfile
 my $header      = 1;
 my $start_dir = getcwd;
 
-while (my $infile = <>) {
-    chomp $infile;
+while (my $infile_1 = <>) {
+    chomp $infile_1;
 
     # Sample input from 'ls $PROJECT/Acey/2019.05/pub_RNAseq_data_03/filt0_RNAseq_reads/*_1.fq';
     # /ocean/projects/mcb190015p/schwarze/Acey/2019.05/pub_RNAseq_data_03/filt0_RNAseq_reads/WashU_adult_fem_01_1.fastq
 
-    if ( $infile =~ /\A (\S+) \/ filt0_RNAseq_reads \/ (\S+) _1\.fq \z/xms ) {
+    if ( $infile_1 =~ /\A (\S+) \/ filt0_RNAseq_reads \/ (\S+) _1\.fq \z/xms ) {
         my $dir_stem  = $1;
         my $file_stem = $2;
         my $outdir = catdir($dir_stem, 'filt2_50nt_RNAseq_reads');
         if (! -r $outdir ) {
             die "Cannot read out: $outdir\n";
         }
-        my $outfile = "$file_stem.filt2.fq.gz";
-        my $outcount = "$file_stem.filt2.fq.readcount.txt";
-        my $json    = "$file_stem.filt2.json";
-        my $html    = "$file_stem.filt2.html";
-        $outfile    = catfile($outdir, $outfile);
-        $outcount    = catfile($outdir, $outcount);
+        my $outfile_1  = "$file_stem.filt2.50nt_1.fq";
+        my $outcount_1 = "$file_stem.filt2.50nt_1.fq.readcount.txt";
+        my $json       = "$file_stem.filt2.50nt.json";
+        my $html       = "$file_stem.filt2.50nt.html";
+        $outfile_1     = catfile($outdir, $outfile_1);
+        $outcount_1    = catfile($outdir, $outcount_1);
 
         # print header lines only once, at the top of the output script
 
@@ -47,12 +47,12 @@ while (my $infile = <>) {
 
         print "fastp --thread 8 --dont_overwrite --json $json --html $html ";
         print ' --n_base_limit 0 --length_required 50 --max_len1 50';
-        print " --in1 $infile --out1 $outfile ;\n";
-        print "zcat $outfile | count_simple_fastq_residues.pl > $outcount ;\n";
+        print " --in1 $infile_1 --out1 $outfile_1 ;\n";
+        print "cat $outfile_1 | count_simple_fastq_residues.pl > $outcount_1 ;\n";
     }
 
     else {
-	die "Cannot parse input line: $infile\n";
+	die "Cannot parse input line: $infile_1\n";
     }
 }
 
