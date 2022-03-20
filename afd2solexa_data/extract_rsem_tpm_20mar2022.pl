45076e2b2f68804264a5a9c7a276d10dcbf416fd
Erich Schwarz <ems394@cornell.edu>
2022-03-20 18:47:54 -0400

    Add extract_rsem_tpm_20mar2022.pl to afd2solexa_data.

---
+++ afd2solexa_data/extract_rsem_tpm_20mar2022.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+use File::Basename;
+
+while (my $input = <>) {
+    chomp $input;
+    my $basename = basename $input;
+    if ( $basename =~ /\A (\S+) \.genes\.results \z/xms ) { 
+        my $stem = $1;
+        my $outfile = $stem . '.pme_TPM.tsv.txt';
+        my $err     = $stem . '.err';
+        print "    cut -f 1,10 $input | perl -ne ' s/gene_id/Gene/; print; ' | wbg2fullnames.pl -n c_elegans.PRJNA13758.WS245.gene_id.tsv.txt -i - 1>$outfile 2>$err ;\n";
+    }
+}
