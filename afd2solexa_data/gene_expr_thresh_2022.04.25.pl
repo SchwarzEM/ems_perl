d92448837f7d66fd612c2af494ef335678f34c14
Erich Schwarz <ems394@cornell.edu>
2022-04-25 17:24:29 -0400

    Revise so that both protein-coding and ncRNA genes above a defined expression threshold are simultaneously tabulated.

--- afd2solexa_data/gene_expr_thresh_2022.04.25.pl
+++ afd2solexa_data/gene_expr_thresh_2022.04.25.pl
@@ -11,6 +11,8 @@ my $data_ref;
 my $infile = $ARGV[0];
 my $thresh = $ARGV[1];
 
+my $header = "Type\tProt.-cod.\tncRNA";
+
 if ( (! $infile) or (! $thresh ) ) {
     die "Format: gene_expr_thresh_2022.04.25.01.pl [infile] [expr. threshold] > [genes per condition]\n";
 }
@@ -25,26 +27,56 @@ my $type_count = 0;
 open my $INFILE, '<', $infile;
 while (my $input = <$INFILE>) {
     chomp $input;
-    if ( $input =~ /\A [^\t\s]+ \t (?: Coding |[^\t]* protein [^\t]*) \t (.+) \z/xms ) {
-        my $type_text = $1;
+
+    if ( $input =~ /\A [^\t\s]+ \t ([^\t]+) \t (.+) \z/xms ) {
+        my $gene_annot = $1;
+        my $type_text  = $2;
+
+        if ( $gene_annot =~ /protein/xms ) {
+            $gene_annot = 'Protein-coding';
+        }
+
         my @types = split /\t/, $type_text;
         $type_count = @types;
         my $array_index = $type_count;
         $array_index--;
         foreach my $i (0..$array_index) {
             my $j = ($i + 1);
-            push @{ $data_ref->{$j} }, $types[$i];
+            push @{ $data_ref->{$j}->{$gene_annot} }, $types[$i];
         }
     }
 }
 
 foreach my $i (1..$type_count) {
-    my @data = @{ $data_ref->{$i} };
-    my $type = shift @data;
-    my @expr = grep { $_ >= $thresh } @data;
-    my $expr_count = @expr;
-    $expr_count = commify($expr_count);
-    print "$type\t$expr_count\n";
+
+    my @protein_data = ();
+    my @ncRNA_data = ();
+    my $type = q{};
+
+    if ( exists $data_ref->{$i}->{'Protein-coding'} ) {
+        @protein_data = @{ $data_ref->{$i}->{'Protein-coding'} };
+    }
+
+    if ( exists $data_ref->{$i}->{'ncRNA'} ) {
+        @ncRNA_data = @{ $data_ref->{$i}->{'ncRNA'} };
+    }
+
+    if ( exists $data_ref->{$i}->{'Coding'} ) {
+        $type = shift @{ $data_ref->{$i}->{'Coding'} };
+    }
+
+    my @protein_expr = grep { $_ >= $thresh } @protein_data;
+    my $protein_expr_count = @protein_expr;
+    $protein_expr_count = commify($protein_expr_count);
+
+    my @ncRNA_expr = grep { $_ >= $thresh } @ncRNA_data;
+    my $ncRNA_expr_count = @ncRNA_expr;
+    $ncRNA_expr_count = commify($ncRNA_expr_count);
+
+    print "$header\n" if $header;
+    $header = q{};
+
+    print "$type\t$protein_expr_count\t$ncRNA_expr_count\n";
 }
 
 
