2506a3577e342c799841e022e4810aa4b34c5e42
Erich Schwarz <ems394@cornell.edu>
2022-08-02 13:30:11 -0400

    Add reject_junk_rnaseq_read-1s_02aug2022.pl and reject_junk_rnaseq_read-2s_02aug2022.pl to Hco_Acey.

---
+++ Hco_Acey/reject_junk_rnaseq_read-2s_02aug2022.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+# This script has one purpose: to enforce clean reformatting of read-1 FASTQ header lines from an unreliable source.  If a FASTQ header line fails to meet very basic criteria, the script dies, and thus saves me a lot of trouble later.
+
+while (my $input = <>) {
+    chomp $input;
+    if ( $input =~ /\A [@] /xms ) {
+        if ( $input =~ /\A [@] \S+ [ ] \d: /xms ) {
+            $input =~ s/\A([@]\S+)[ ]\d:/$1\#0\/2 2:/;
+            print "$input\n";
+        }
+        else {
+            die "Misformatted FASTQ header line: $input\n";
+        }
+    }
+    else {
+        print "$input\n";
+    }
+}
+
