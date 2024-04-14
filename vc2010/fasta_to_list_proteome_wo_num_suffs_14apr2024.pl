c01cc5b6095cf37bc1a410bb50cacc1f6ea5bd93
Erich Schwarz <ems394@cornell.edu>
2024-04-14 16:16:42 -0400

    Add add_gene_col_14apr2024.pl and fasta_to_list_proteome_wo_num_suffs_14apr2024.pl to vc2010.

---
+++ vc2010/fasta_to_list_proteome_wo_num_suffs_14apr2024.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $proteome    = q{};
+$proteome       = $ARGV[0] if $ARGV[0];
+
+my $prot_id     = q{};
+my $prot_seq    = q{};
+
+if (! $proteome ) {
+    die "Format: prots_to_nonred_seqs_11jul2023.pl [proteome] => [seq name (tab) aa seq in one text line]\n";
+}
+
+open my $PROTEOME, '<', $proteome;
+while (my $input = <$PROTEOME> ) {
+    chomp $input;
+    if ( $input =~ /\A [>] (\S+) /xms ) {
+        my $new_prot_id = $1;
+        if ( $prot_id and $prot_seq ) {
+            print "$prot_id\t$prot_seq\n";
+        }
+        $new_prot_id =~ s/(\.\d+[a-z]*)\.\d+\z/$1/;
+        $prot_id  = $new_prot_id;
+        $prot_seq = q{};
+    }
+    elsif ($prot_id) {
+        $input =~ s/\s//;
+        $prot_seq = $prot_seq . $input;
+    }
+}
+close $PROTEOME;
+
+# Finish recording any remaining sequence data:
+if ( $prot_id and $prot_seq ) {
+    print "$prot_id\t$prot_seq\n";
+}
+
