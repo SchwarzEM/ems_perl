d8dd2f98218a73a543181713d68e88148cc17a0c
Erich Schwarz <ems394@cornell.edu>
2024-04-14 18:16:04 -0400

    Modify to deal with _\d+ suffixes from extra copies from LiftOff.

---
+++ vc2010/add_gene_col_14apr2024.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $cds2g_tab = q{};
+my $cds_tab   = q{};
+
+$cds2g_tab = $ARGV[0] if $ARGV[0];
+$cds_tab   = $ARGV[1] if $ARGV[1];
+
+my %cds2gene = ();
+
+if ( (! $cds2g_tab ) or (! $cds_tab ) ) {
+    die "Format: add_gene_col_14apr2024.pl [cds2gene table] [cds + something table] > [cds + gene + something table]\n";
+}
+
+open my $CDS2G_TAB, '<', $cds2g_tab;
+while ( my $input = <$CDS2G_TAB> ) {
+    chomp $input;
+    if ( $input =~ /\A (\S+) \t (\S+) \z/xms ) {
+        my $cds  = $1;
+        my $gene = $2;
+        if ( exists $cds2gene{$cds} ) {
+            die "Redundant mapping of CDS $cds to both $cds2gene{$cds} and $gene\n";
+        }
+        $cds2gene{$cds} = $gene;
+    }
+    else {
+        die "From cgc2gene table $cds2g_tab, cannot parse: $input\n";
+    }
+}
+close $CDS2G_TAB;
+
+open my $CDS_TAB, '<', $cds_tab;
+while ( my $input = <$CDS_TAB> ) {
+    chomp $input; 
+    if ( $input =~ /\A (\S+) \t (.+) \z/xms ) { 
+        my $cds   = $1;
+        my $annot = $2;
+        my $gene  = q{};
+        if (! exists $cds2gene{$cds} ) {
+            my $alt_cds = $cds;
+            $alt_cds =~ s/\.\d+_\d+\z//;
+
+            if (! exists $cds2gene{$alt_cds} ) {
+                die "Cannot map CDS $cds to a gene\n";
+            }
+            $gene = $cds2gene{$alt_cds};
+
+            my $suffix = q{};
+            if ( $cds =~ /\A \S+ (_\d+) \z/xms ) {
+                $suffix = $1;
+            }
+            else {
+                die "Cannot extract suffix from CDS $cds\n";
+            }
+
+            # The CDS name needs to be stripped of the extra '.1+' that it gets from gffread.
+            if ( $cds =~ /\A (\S+) \.\d+ (_\d+) \z/xms ) {
+                $cds = "$1$2";
+            }
+            # The gene name needs to be revised human-readably.
+            $gene = $gene . '.extra_copy' . $suffix;
+        }
+        else {
+            $gene = $cds2gene{$cds};
+        }
+        print "$cds\t$gene\t$annot\n";
+    }
+    else {
+        die "From CDS-annotation table $cds_tab, cannot parse: $input\n";
+    }
+}
+close $CDS_TAB;
+
