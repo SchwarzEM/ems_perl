a4555cc084086a20ebd2c963431e734ba915f355
Erich Schwarz <ems394@cornell.edu>
2024-08-27 17:55:19 -0400

    Add consolidate_annots_27aug2024.pl on vc2010.

---
+++ vc2010/consolidate_annots_27aug2024.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use autodie;
+use strict;
+
+use List::MoreUtils qw(uniq);
+
+my @genes = ();
+my $data_ref;
+
+while (my $input = <> ) {
+    chomp $input;
+    if ( $input =~ /\A (\S+) \t (\S+) \z/xms ) {
+        my $gene  = $1;
+        my $annot = $2;
+        push @genes, $gene;
+        $data_ref->{'gene'}->{$gene}->{'annot'}->{$annot} = 1;
+    }
+    else {
+        die "Cannot parse input: $input\n";
+    }
+}
+
+@genes = uniq(@genes);
+
+foreach my $gene (@genes) {
+    if ( exists $data_ref->{'gene'}->{$gene}->{'annot'} ) {
+        my @annots = sort keys %{ $data_ref->{'gene'}->{$gene}->{'annot'} };
+        my $annot_text = join '; ', @annots;
+        print "$gene\t$annot_text\n";
+    }
+}
+
