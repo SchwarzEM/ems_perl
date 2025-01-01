e161f03ec6d187c1b0ea5fd2864deaa56cf9d24f
Erich Schwarz <ems394@cornell.edu>
2025-01-01 00:14:37 -0500

    Add make_prebools_31dec2024b.pl to Hco_Acey.

---
+++ Hco_Acey/make_prebools_31dec2024b.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $i = 0;
+
+while ( my $input = <> ) {
+    chomp $input;
+    if ( $input =~ /\A (\S+) \.clusts\.\S+\.tsv.txt/xms ) {
+        my $tag = $1;
+        my $outfile = 'Sherm.ofind_16spp.vs.' . $tag . '.tsv.txt';
+        $i++;
+        print 'add_tab_annots.pl -f ',
+              '-i ../annots/Sherm_2024.09.30.genes.txt ',
+              '-a ../annots/Sherm_summary_orthofinder_16spp_2024.12.17.01.tsv.txt ',
+              "$input ",
+              '1>',
+              "$outfile ",
+              '2>',
+              "test$i.err ;",
+              "\n",
+              ;
+    }
+    else {
+        die "Cannot parse input: $input\n";
+    }
+}
