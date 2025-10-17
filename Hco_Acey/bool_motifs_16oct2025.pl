274f4a768a853f75706781e1e3be58f240353b12
Erich Schwarz <ems394@cornell.edu>
2025-10-16 23:50:53 -0400

    Add third argument to provide human-readable motif name to the header of the output Boolean table.

--- Hco_Acey/bool_motifs_16oct2025.pl
+++ Hco_Acey/bool_motifs_16oct2025.pl
@@ -6,12 +6,14 @@ use autodie;
 
 my $infile = q{};
 my $motif  = q{};
+my $name   = q{};   # the most reliable way to replace a motif with a human-readable motif name is to do it in the command line!
 
 $infile = $ARGV[0] if $ARGV[0];
 $motif  = $ARGV[1] if $ARGV[1];
+$name   = $ARGV[2] if $ARGV[2];
 
-if ( (! $infile ) or (! $motif ) ) {
-    die "Format: bool_motifs_16oct2025.pl [gene-motif TSV] [single_motif] > [Booleanized gene-single_motif TSV]\n";
+if ( (! $infile ) or (! $motif ) or (! $name ) ) {
+    die "Format: bool_motifs_16oct2025.pl [gene-motif TSV] [single_motif] [human-readable motif name] > [Booleanized gene-readable_single_motif TSV]\n";
 }
 
 open my $INFILE, '<', $infile;
@@ -19,7 +21,7 @@ while ( my $input = <$INFILE> ) {
     my $output = q{};
     chomp $input;
     if ( $input =~ /\A Gene \t [^\t]+ \z/xms ) {
-        $output = "Gene\t$motif";
+        $output = "Gene\t$name";
     }
     elsif ( $input =~ /\A (\S+) \t [^\t]* $motif [^\t]* \z/xms ) {
         my $gene = $1;
