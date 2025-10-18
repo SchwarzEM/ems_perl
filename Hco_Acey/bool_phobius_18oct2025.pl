1ebb091b0b24fa6edc70b4a0d0396e0bb5cda88f
Erich Schwarz <ems394@cornell.edu>
2025-10-18 18:57:08 -0400

    Add bool_phobius_18oct2025.pl to Hco_Acey.

---
+++ Hco_Acey/bool_phobius_18oct2025.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $infile  = q{};
+my $phobius = q{};
+
+$infile  = $ARGV[0] if $ARGV[0];
+$phobius = $ARGV[1] if $ARGV[1];
+
+if ( (! $infile ) or (! $phobius ) ) {
+    die "Format: bool_phobius_18oct2025.pl [gene-Phobius annots.] [spec. Phobius annot.] > [Boolean table for spec. Phobius annot.]\n";
+}
+
+open my $INFILE, '<', $infile;
+while ( my $input = <$INFILE> ) {
+    chomp $input;
+    if ( ( $input !~ /\AGene\t/xms ) and ( $input =~ /\A (\S+) \t/xms ) ) {
+        my $gene = $1;
+        if ( ( $input =~ /\A \S+ \t .* \b $phobius [;] .* /xms ) or ( $input =~ /\A \S+ \t .* \b $phobius \z/xms ) ) {
+            $input = "$gene\ttrue";
+        }
+        else {
+            $input = "$gene\tfalse";
+        }
+    }
+    print "$input\n";
+}
+close $INFILE;
+
+
