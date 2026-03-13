2170e0291988cd5d1eb013275573e6f6d05edb4b
Erich Schwarz <ems394@cornell.edu>
2026-03-13 19:14:26 -0400

    Add make_snpden2bed_13mar2026a.pl to Hco_Acey.

--- Hco_Acey/make_snpden2bed_13mar2026a.pl
+++ Hco_Acey/make_snpden2bed_13mar2026a.pl
@@ -4,17 +4,31 @@ use strict;
 use warnings;
 use autodie;
 
-while ( my $infile = <> ) {
+use Scalar::Util qw(looks_like_number);
+
+my $list = q{};
+my $bin  = q{};
+
+$list = $ARGV[0] if $ARGV[0];
+$bin  = $ARGV[1] if $ARGV[1];
+
+if ( (! $list ) or (! $bin ) or (! looks_like_number($bin) ) ) {
+    die "Format: make_snpden2bed_13mar2026a.pl [list of input snpden files] [bin size] > [snpden2bed_13mar2026.pl commands]\n";
+}
+
+open my $LIST, '<', $list;
+while ( my $infile = <$LIST> ) {
     chomp $infile;
     if ( $infile =~ /\A (\S+) \. snpden \z/xms ) {
         my $stem = $1;
         my $outfile = "$stem.bed";
         my $err     = "$stem.err";
         print '$PROJECT/ems_perl/Hco_Acey/snpden2bed_13mar2026.pl ';
-        print "$infile 1>$outfile 2>$err ;\n";
+        print "$infile $bin 1>$outfile 2>$err ;\n";
     }
     else {
         die "Cannot parse infile: $infile\n";
     }
 }
+close $LIST;
 
