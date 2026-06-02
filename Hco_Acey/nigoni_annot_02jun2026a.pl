d1ab087ddaf5818d1a0f025696a51577bd2ece1f
Erich Schwarz <ems394@cornell.edu>
2026-06-02 16:32:03 -0400

    Add nigoni_annot_02jun2026a.pl to Hco_Acey.

---
+++ Hco_Acey/nigoni_annot_02jun2026a.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+use Scalar::Util qw(looks_like_number);
+
+while (my $input = <> ) {
+    chomp $input;
+    if ( $input =~ /\A Gene \t/xms ) {
+        print "Gene\tSex-expr-bias\n";
+    }
+    elsif ( $input =~ /\A (\S+) \t ([^\t]*) \t ([^\t]*) \z/xms ) {
+        my $gene = $1;
+        my $fc   = $2;
+        my $fdr  = $3;
+        if ( ( looks_like_number($fc) ) and ( looks_like_number($fdr) ) and ( $fdr <= 0.001 ) ) {
+            if ( $fc >= 1 ) {
+                print "Gene\tMale_bias\n";
+            }
+            elsif ( $fc <= -1 ) {
+                print "Gene\tFem_bias\n";
+            }
+            else {
+                print "Gene\tNo_bias\n";
+            }
+        }
+        else {
+            print "Gene\tNo_bias\n";
+        }
+    }
+    else {
+        die "Cannot parse: $input\n";
+    }
+}
