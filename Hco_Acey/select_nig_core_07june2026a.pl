7f55dc36da60fc4d580ff5f5fe41d8f5dbf3fcc8
Erich Schwarz <ems394@cornell.edu>
2026-06-07 17:06:27 -0400

    Add select_nig_core_07june2026a.pl and select_nig_core_07june2026b.pl to Hco_Acey.

---
+++ Hco_Acey/select_nig_core_07june2026a.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+while ( my $input = <> ) {
+    chomp $input;
+    if (     ( $input =~ /HKU_nigoni_JU1422/xms ) 
+         and ( $input =~ /nigoni_EG5268/xms )
+         and ( $input =~ /nigoni_JU1421/xms )
+         and ( $input =~ /nigoni_JU2484/xms )
+         and ( $input =~ /nigoni_JU2617/xms )
+         and ( $input =~ /nigoni_JU4356/xms )
+         and ( $input =~ /nigoni_VSL2202/xms )
+         and ( $input =~ /nigoni_YR106/xms )
+         and ( $input =~ /nigoni_ZF1220/xms )
+         and ( $input =~ /orig_nigoni_JU1422/xms )
+         and ($input !~ /briggsae_AF16/xms )
+         and ($input !~ /briggsae_ED3036/xms )
+         and ($input !~ /briggsae_HK104/xms )
+         and ($input !~ /briggsae_JU349/xms )
+         and ($input !~ /briggsae_QR24/xms )
+         and ($input !~ /briggsae_QX1410/xms )
+         and ($input !~ /briggsae_VX34/xms )
+       ) {
+        print "$input\n";
+    }
+}
