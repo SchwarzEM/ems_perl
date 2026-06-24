6c0a15df4052993cc37374f49d7c66acabf90651
Erich Schwarz <ems394@cornell.edu>
2026-06-24 13:54:14 -0400

    Add select_pg_taxa_24jun2026a.pl and select_pg_taxa_24jun2026b.pl to Hco_Acey.

---
+++ Hco_Acey/select_pg_taxa_24jun2026b.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+while ( my $input = <> ) { 
+    chomp $input;
+    if ( $input =~ /\A Gene \t/xms ) {
+        print "$input\n";
+    }
+    elsif ( $input =~ /\A [^\t]+ \t [^\t]+ \t ([^\t]+) \t ([^\t]*) \t ([^\t]*) /xms ) {
+        my $pass_taxa   = $1;
+        my $array_taxa  = $2;
+        my $nonsyn_taxa = $3;
+        if (     ( $pass_taxa =~ /Aroian/xms ) 
+             and ( $pass_taxa =~ /(Oita|obscurus)/xms ) 
+             and ( $array_taxa eq 'Baylor; Keiser' )
+           ) {
+            print "$input\n";
+        }
+    }
+    else {
+        die "Cannot parse: $input\n";
+    }
+}
+
