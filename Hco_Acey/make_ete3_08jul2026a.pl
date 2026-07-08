7845ad1fa1c2f4b0d1981ba45d2f83f47837d095
Erich Schwarz <ems394@cornell.edu>
2026-07-08 17:24:31 -0400

    Add make_ete3_08jul2026a.pl to Hco_Acey.

---
+++ Hco_Acey/make_ete3_08jul2026a.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use autodie;
+
+my $header = "import ete3\nfrom ete3 import Tree\n";
+
+while (my $input = <> ) {
+    chomp $input;
+    my $i          = 0;
+    my $taxon_text = q{};
+    if ( $input =~ /\A (\d+) \t ([^\t]+) \z/xms ) {
+        $i          = $1;
+        $taxon_text = $2;
+
+        $taxon_text =~ s/\A\s+//;
+        $taxon_text =~ s/\s+\z//; 
+
+        my @taxa = map { "\"$_\"" } split '; ', $taxon_text;
+        my $out_tax_text = join ', ', @taxa;
+
+        my $out_tree = "orthofinder.$i.pruned.tree.nwk";
+
+        print $header if $header;
+        $header = q{};
+
+        print 'tree = Tree("orthofinder.1.tree.v2.nwk")', "\n";
+        print 'alignment_taxa = {', $out_tax_text, '}', "\n";
+        print 'tree.prune(alignment_taxa, preserve_branch_length=True)', "\n";
+        print 'tree.write(outfile="', $out_tree, '", format=1)', "\n";
+    }
+    else {
+        die "Cannot parse: $input\n";
+    }
+}
+
+print "quit()\n";
